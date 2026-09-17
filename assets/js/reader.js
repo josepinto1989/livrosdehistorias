57f5f95ff4349e769f36b6bf798aa314dc549202
josepinto1989 <sequeirapinto@gmail.com>
2026-09-17 21:41:47 +0100

    Add Cão Joaquim skydiving story

--- assets/js/reader.js
+++ assets/js/reader.js
@@ -1,6 +1,6 @@
 
 const params = new URLSearchParams(window.location.search);
-const assetVersion = "20260905-2";
+const assetVersion = "20260917-1";
 const requestedBookId = params.get("book") || "cao-joaquim-submarino";
 const bookId = /^[a-z0-9-]+$/.test(requestedBookId) ? requestedBookId : "cao-joaquim-submarino";
 
